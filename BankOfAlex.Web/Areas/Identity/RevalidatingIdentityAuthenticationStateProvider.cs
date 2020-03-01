8e6fd15e9619c8456e2e3b88530f954cff1d99b5
Alex Pope <alexander.s.pope@gmail.com>
2020-03-01 09:50:17 -0500

    Customize Blazor project to use PostgreSQL and remove unneeded templated items.

--- BankOfAlex.Web/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ BankOfAlex.Web/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -1,27 +1,25 @@
-using System;
-using System.Security.Claims;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BankOfAlex.Web.Areas.Identity
 {
-    public class RevalidatingIdentityAuthenticationStateProvider<TUser>
-        : RevalidatingServerAuthenticationStateProvider where TUser : class
+    public class RevalidatingIdentityAuthenticationStateProvider<TUser> : RevalidatingServerAuthenticationStateProvider
+        where TUser : class
     {
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly IdentityOptions _options;
 
-        public RevalidatingIdentityAuthenticationStateProvider(
-            ILoggerFactory loggerFactory,
-            IServiceScopeFactory scopeFactory,
-            IOptions<IdentityOptions> optionsAccessor)
+        public RevalidatingIdentityAuthenticationStateProvider(ILoggerFactory loggerFactory,
+                                                               IServiceScopeFactory scopeFactory,
+                                                               IOptions<IdentityOptions> optionsAccessor)
             : base(loggerFactory)
         {
             _scopeFactory = scopeFactory;
@@ -30,14 +28,15 @@ namespace BankOfAlex.Web.Areas.Identity
 
         protected override TimeSpan RevalidationInterval => TimeSpan.FromMinutes(30);
 
-        protected override async Task<bool> ValidateAuthenticationStateAsync(
-            AuthenticationState authenticationState, CancellationToken cancellationToken)
+        protected override async Task<bool> ValidateAuthenticationStateAsync(AuthenticationState authenticationState,
+                                                                             CancellationToken cancellationToken)
         {
-            // Get the user manager from a new scope to ensure it fetches fresh data
             var scope = _scopeFactory.CreateScope();
+
             try
             {
                 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+
                 return await ValidateSecurityStampAsync(userManager, authenticationState.User);
             }
             finally
@@ -53,9 +52,11 @@ namespace BankOfAlex.Web.Areas.Identity
             }
         }
 
-        private async Task<bool> ValidateSecurityStampAsync(UserManager<TUser> userManager, ClaimsPrincipal principal)
+        private async Task<bool> ValidateSecurityStampAsync(UserManager<TUser> userManager,
+                                                            ClaimsPrincipal principal)
         {
             var user = await userManager.GetUserAsync(principal);
+
             if (user == null)
             {
                 return false;
@@ -68,6 +69,7 @@ namespace BankOfAlex.Web.Areas.Identity
             {
                 var principalStamp = principal.FindFirstValue(_options.ClaimsIdentity.SecurityStampClaimType);
                 var userStamp = await userManager.GetSecurityStampAsync(user);
+
                 return principalStamp == userStamp;
             }
         }
--- BankOfAlex.Web/Program.cs
+++ BankOfAlex.Web/Program.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using BankOfAlex.Web.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace BankOfAlex.Web
 {
@@ -15,14 +14,37 @@ namespace BankOfAlex.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build())
+                .UseStartup<Startup>()
+                .Build();
+        }
     }
 }
